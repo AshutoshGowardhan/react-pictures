780a65652d7a44344a60e0806557f516733782cc
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-25 23:04:24 +0530

    binding call back same error, this.setState is not a function

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,23 @@ import axios from 'axios';
 
 class App extends React.Component {
 
-    async onSearchSubmit(term) {
-        console.log(term)
+    state = { images: [] };
+
+     onSearchSubmit = async (term) => {
         let resp = await axios.get('https://api.unsplash.com/search/photos',{
             params: { query:term },
             headers : {
-                
                 Authorization: 'Client-ID 9T6bbmEgUlehixtuTwanEO_X5JOX43d_e7-8k-THFBc'
             }
         });
-        console.log(resp.data.results)
+        this.setState({images: resp.data.results})
     }
 
     render() {
         return (
             <div className="ui container" style={{margin: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found : {this.state.images.length} images
             </div>
         )
     }
