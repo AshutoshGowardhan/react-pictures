dee45ab11775202fbeb1530c5c0cc1a2a17197c6
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-26 16:19:52 +0530

    rendering the list and adding a unique key to every dom element

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    console.log(props.images)
+    const images = props.images.map((image)=> {
+        return <img key={image.id} src={image.urls.regular} alt="ok"></img>
+    })
     return (
-        <div>Image List</div>
+        <div>{images}</div>
     );
 }
 
